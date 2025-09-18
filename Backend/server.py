3928111920eb009c34197e59344b1402848da89c
Asif Uddin Ahmed <mylifeasasif@gmail.com>
2025-09-19 00:03:59 +0600

    port fixed

--- Backend/server.py
+++ Backend/server.py
@@ -1,7 +1,7 @@
-import uvicorn
+import os
 from src.app import app
+import uvicorn
 
 if __name__ == "__main__":
-    import uvicorn
-
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=8000)
--- Backend/src/app.py
+++ Backend/src/app.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ['*'],
+    allow_origins = ['https://uiu-ibol-demo.vercel.app/', '*'],
     allow_credentials = True,
     allow_methods = ['*'],
     allow_headers = ['*']
